dc9ef065336ec33875db95358955b64e83fca3fe
PhilV1 <werpub.phil@gmx.de>
2023-11-08 12:14:15 +0100

    added files

---
+++ script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const dicePlayer = document.getElementById('dicePlayer');
+const diceComputer = document.getElementById('diceComputer');
+const diceRoll = document.getElementById('roll');
+const message = document.getElementById('message');
+const playerScoreboard = document.getElementById('playerScore');
+const computerScoreboard = document.getElementById('computerScore');
+const playAgain = document.querySelector('.playAgain');
+const playerName = document.getElementById('playerName');
+const computerName = document.getElementById('computerName');
+console.log(playerName, computerName);
+
+// Play Again
+const resetGame = function () {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreboard.textContent = playerScore;
+  computerScoreboard.textContent = computerScore;
+};
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Dice Roll
+diceRoll.addEventListener('click', function () {
+  let playerChoice = Math.trunc(Math.random() * 6) + 1;
+  let computerChoice = Math.trunc(Math.random() * 6) + 1;
+  dicePlayer.src = `./images/dice${playerChoice}.png`;
+  diceComputer.src = `./images/dice${computerChoice}.png`;
+
+  if (playerChoice > computerChoice) {
+    if (playerScore < 4) {
+      playerScore++;
+      playerScoreboard.textContent = playerScore;
+      console.log(playerScore);
+      message.textContent = 'You won!';
+      document.querySelector('.player').classList.add('darkgrey');
+      document.querySelector('.computer').classList.remove('darkgrey');
+      playerName.classList.add('nameColor');
+      computerName.classList.remove('nameColor');
+    } else {
+      console.log('You won the game');
+      document.getElementById('roll').style.display = 'none';
+      playerScore++;
+      playerScoreboard.textContent = playerScore;
+      message.textContent = 'You won the game!';
+      playAgain.classList.remove('hidden');
+      message.textContent = 'Congratulation, you won! Wanna play again?';
+      document.querySelector('.player').classList.add('darkgrey');
+      document.querySelector('.computer').classList.remove('darkgrey');
+      playerName.classList.add('nameColor');
+      computerName.classList.remove('nameColor');
+    }
+  } else if (computerChoice > playerChoice) {
+    if (computerScore < 4) {
+      computerScore++;
+      computerScoreboard.textContent = computerScore;
+      console.log(computerScore);
+      message.textContent = 'You lost';
+      document.querySelector('.computer').classList.add('darkgrey');
+      document.querySelector('.player').classList.remove('darkgrey');
+      computerName.classList.add('nameColor');
+      playerName.classList.remove('nameColor');
+    } else {
+      console.log('You lost the game');
+      document.getElementById('roll').style.display = 'none';
+      computerScore++;
+      computerScoreboard.textContent = computerScore;
+      message.textContent = 'You lost.';
+      playAgain.classList.remove('hidden');
+      message.textContent = 'You lost the game. Better luck next time.';
+      document.querySelector('.computer').classList.add('darkgrey');
+      document.querySelector('.player').classList.remove('darkgrey');
+      computerName.classList.add('nameColor');
+      playerName.classList.remove('nameColor');
+    }
+  } else {
+    console.log('It is a draw');
+    message.textContent = 'It is a draw';
+    document.querySelector('.computer').classList.remove('darkgrey');
+    document.querySelector('.player').classList.remove('darkgrey');
+    computerName.classList.remove('nameColor');
+    playerName.classList.remove('nameColor');
+  }
+});
+
+playAgain.addEventListener('click', function () {
+  resetGame();
+  playAgain.classList.add('hidden');
+  document.getElementById('roll').style.display = '';
+  message.textContent = 'Press "Roll" to start the game.';
+  document.querySelector('.computer').classList.remove('darkgrey');
+  document.querySelector('.player').classList.remove('darkgrey');
+  computerName.classList.remove('nameColor');
+  playerName.classList.remove('nameColor');
+});
